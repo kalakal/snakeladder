ceaa976f14a75969ef4ccbde9881aeae6d1be0d0
kalakal <sayedfaizanqadri@gmail.com>
2022-11-04 22:36:44 +0530

    snake and ladder added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
+            Console.Read();
+        }
+
+        public int PlayerPosition = 0;
+    }
+}
+
